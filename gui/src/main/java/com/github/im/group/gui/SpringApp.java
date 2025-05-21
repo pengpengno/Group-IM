b161cd6e6fd467804f1860f69253d5285b06be91
peng.wang <Allen.pen.on1@gmail.com>
2025-05-21 14:04:39 +0800

    修复 Graalvm 编译异常 : 取消webmvc 自动注入

--- gui/src/main/java/com/github/im/group/gui/SpringApp.java
+++ gui/src/main/java/com/github/im/group/gui/SpringApp.java
@@ -2,6 +2,7 @@ package com.github.im.group.gui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
 /**
  * Description:
@@ -12,7 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  * @since 2025/5/21
  */
-@SpringBootApplication
+// 不需要注入mvc
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
 public class SpringApp {
 
     public static void main(String[] args) {
